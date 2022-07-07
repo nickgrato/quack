234cd22a261660c58bfb2ccee79b8352ab476de7
Nick Grato <nickgrato@ngrato-27545.local>
2022-07-07 16:33:09 -0700

    adding themeing and adding story book functionality

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,77 @@
-import React from "react"
+import React, { MouseEventHandler, useEffect, useState } from 'react';
+import { ButtonT, ButtonCategoriesE, ButtonSizesE } from '../../types/Form';
+import { IconT } from '../../types/General';
+import styles from './Button.module.scss';
+import Icon from '../Icon/Icon';
 
 export type ButtonPropsT = {
-  label: string
-}
+  active?: boolean;
+  id?: string;
+  text?: string;
+  type?: ButtonT;
+  category?: ButtonCategoriesE;
+  size?: ButtonSizesE;
+  disabled?: boolean;
+  icon?: IconT;
+  iconPlacedRight?: boolean;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+  classProp?: string;
+};
 
-const Button = ({ label }: ButtonPropsT) => {
-  return  (<button style={{backgroundColor:'green', color: 'white', padding: '20'}}>{label}</button>)
-}
+const Button = ({
+  active,
+  id,
+  text,
+  type = 'button',
+  category = ButtonCategoriesE.PRIMARY_SOLID,
+  size = ButtonSizesE.MEDIUM,
+  disabled,
+  icon,
+  onClick,
+  iconPlacedRight = false,
+  classProp = '',
+}: ButtonPropsT) => {
+  return (
+    <button
+      className={`
+        ${styles['button_' + category]} 
+        ${styles[size]} 
+        ${!text && styles[size + '_round']} 
+        ${classProp} 
+        ${active && styles['button_' + category + '_active']}
+      `}
+      id={id}
+      aria-label={text}
+      type={type}
+      disabled={disabled}
+      onClick={onClick}
+    >
+      {/* Left Icon  */}
+      {icon && !iconPlacedRight ? (
+        <Icon
+          name={icon}
+          color="currentColor"
+          size={24}
+          classProp={text ? 'margin-right-10' : ''}
+        />
+      ) : (
+        ''
+      )}
+      {/* Button Text  */}
+      {text}
+      {/* Right Icon  */}
+      {icon && iconPlacedRight ? (
+        <Icon
+          name={icon}
+          color="currentColor"
+          size={24}
+          classProp={text ? 'margin-left-10' : ''}
+        />
+      ) : (
+        ''
+      )}
+    </button>
+  );
+};
 
-
-export default Button
+export default Button;
